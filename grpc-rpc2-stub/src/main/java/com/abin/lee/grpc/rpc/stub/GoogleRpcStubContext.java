92ebdd2eb9b88ee44e1359039b18d62a64ea89a0
zondahuman <zondahuman@gmail.com>
2017-09-14 21:02:07 +0800

    add

--- grpc-rpc2-stub/src/main/java/com/abin/lee/grpc/rpc/stub/GoogleRpcStubContext.java
+++ grpc-rpc2-stub/src/main/java/com/abin/lee/grpc/rpc/stub/GoogleRpcStubContext.java
@@ -1,24 +1,38 @@
 package com.abin.lee.grpc.rpc.stub;
 
 
+import com.abin.lee.grpc.rpc.common.context.SpringContextUtils;
+import com.abin.lee.grpc.rpc.stub.common.GoogleRpcRemoteAddress;
 import com.abin.lee.grpc.rpc.stub.common.GoogleRpcStubFactory;
+import io.grpc.BindableService;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NettyChannelBuilder;
+import org.apache.thrift.TServiceClient;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.io.Closeable;
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * 服务端注册服务工厂
  */
 @Component
-public class GoogleRpcStubContext implements  InitializingBean, Closeable, Ordered {
-    private String service;
-    private GoogleRpcStubFactory stubFactory;
+public class GoogleRpcStubContext implements FactoryBean, InitializingBean, Closeable, Ordered {
     private Object proxyClient;
     private Class<?> objectClass;
 
+    ManagedChannel channel = NettyChannelBuilder.forAddress("localhost", 10086).usePlaintext(true).build();
+    @Resource
+    GoogleRpcRemoteAddress googleRpcRemoteAddress;
 
     @Override
     public void close() throws IOException {
@@ -28,37 +42,66 @@ public class GoogleRpcStubContext implements  InitializingBean, Closeable, Order
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        service = stubFactory.getService().toString();
-        // 加载Iface接口
-        objectClass = classLoader.loadClass(service);
-        String grpcName = objectClass.getSimpleName();
-        String allName = objectClass.getName();
-        grpcName = grpcName.substring(grpcName.lastIndexOf("."), grpcName.indexOf("Grpc"));
-        Class<?> targetClass = classLoader.loadClass(allName + "$"+grpcName+"BlockingStub");
+        channel = NettyChannelBuilder.forAddress(googleRpcRemoteAddress.getHost(), googleRpcRemoteAddress.getPort()).usePlaintext(true).build();
+        Object service = null;
+        Map<String, GoogleRpcStubFactory> handlers = SpringContextUtils.getBeansOfType(GoogleRpcStubFactory.class);
+        for (Iterator<Map.Entry<String, GoogleRpcStubFactory>> iterator = handlers.entrySet().iterator(); iterator.hasNext(); ) {
+            Map.Entry<String, GoogleRpcStubFactory> entry = iterator.next();
+            String beanName = entry.getKey();
+            GoogleRpcStubFactory instance = entry.getValue();
+            service = instance.getService();
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//        service = stubFactory.getService().toString();
+            // 加载Iface接口
+            objectClass = classLoader.loadClass((String)service);
+            String grpcName = objectClass.getSimpleName();
+            String allName = objectClass.getName();
+            String prefixName = grpcName.substring(0, grpcName.indexOf("Grpc"));
+            Class<?> targetClass = classLoader.loadClass(allName + "$"+prefixName+"BlockingStub");
 
-        System.out.println("targetClass="+targetClass);
+            System.out.println("targetClass="+targetClass);
+
+            proxyClient = Proxy.newProxyInstance(classLoader, new Class[] { objectClass }, new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    //
+                    TServiceClient client = pool.borrowObject();
+                    boolean flag = true;
+                    try {
+                        System.out.println("objectClass="+objectClass);
+                        System.out.println("client.getClass().getName()="+ client.getClass().getName());
+                        return method.invoke(client, args);
+                    } catch (Exception e) {
+                        flag = false;
+                        throw e;
+                    } finally {
+
+                    }
+                }
+            });
+
+        }
+
+
 //        BindableService
 
 
     }
 
 
-
-    public String getService() {
-        return service;
+    @Override
+    public Object getObject() throws Exception {
+        return proxyClient;
     }
 
-    public void setService(String service) {
-        this.service = service;
+    @Override
+    public Class<?> getObjectType() {
+        return objectClass;
     }
 
-    public GoogleRpcStubFactory getStubFactory() {
-        return stubFactory;
-    }
-
-    public void setStubFactory(GoogleRpcStubFactory stubFactory) {
-        this.stubFactory = stubFactory;
+    @Override
+    public boolean isSingleton() {
+        return false;
     }
 
     @Override
